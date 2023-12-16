19034ab426423ce1404261c73f3031911298f470
Leonid <76591317+Leonid578@users.noreply.github.com>
2023-12-16 21:47:16 +0200

    upd 2

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,16 @@
-import First from './MyFirstThree.js/firstExperience';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
 
-  function App() {
-    return (
+import ExaminationOne from "./MyFirstThree.js/firstExperience";
+import ExaminationTwo from "./MyTwoThree.js/twoExperience";
+import "./App.css";
+
+function App() {
+  return (
     <div className="App">
-      <First/>
+      <Routes>
+        <Route path="/One" element={<ExaminationOne />}></Route>
+        <Route path="/Two" element={<ExaminationTwo />}></Route>
+      </Routes>
     </div>
   );
 }
